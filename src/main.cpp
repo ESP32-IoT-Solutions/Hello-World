3dee4f1b99ac6bafdf4a113d7fd03d2f22af5099
Maurizio Casciano <mauriziocasciano7@gmail.com>
2023-12-31 11:30:36 +0100

    Print ESP32 Info

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,30 @@
 void setup()
 {
   Serial.begin(115200);
+
+  Serial.print("\nChip Model: ");
+  Serial.println(ESP.getChipModel());
+
+  Serial.print("Chip Cores: ");
+  Serial.println(ESP.getChipCores());
+
+  Serial.print("CPU Frequency: ");
+  Serial.print(ESP.getCpuFreqMHz());
+  Serial.println(" MHz");
+
+  Serial.print("PSRAM: ");
+  Serial.print(ESP.getPsramSize() / (1024.0 * 1024.0));
+  Serial.println(" MB");
+
+  Serial.print("Heap: ");
+  Serial.print(ESP.getHeapSize() / (1024.0 * 1024.0));
+  Serial.println(" MB");
+
+  Serial.print("Flash Memory: ");
+  Serial.print(ESP.getFlashChipSize() / (1024.0 * 1024.0));
+  Serial.println(" MB");
 }
 
 void loop()
 {
-  Serial.println("Hello World");
-  delay(2000);
 }
